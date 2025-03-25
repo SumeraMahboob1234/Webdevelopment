6af80590b0099efbba432fd6b25b26791819eac9
SumeraMahboob1234 <sumeramahboob62@gmail.com>
2025-03-25 17:47:56 +0530

    Initial commit

---
+++ my/ecommerce_backend/orders/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+from products.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=20, choices=[('Pending', 'Pending'), ('Completed', 'Completed')], default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
---
+++ my/ecommerce_backend/users/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import UserDetail
+
+urlpatterns = [
+    path('<int:pk>/', UserDetail.as_view(), name="user-detail"),
+]
